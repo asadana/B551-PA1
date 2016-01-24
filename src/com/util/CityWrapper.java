e1ebb546e18f9751ad30d4a6cafcb2b7e8b048b0
Ankit Sadana <sadana.ankit@gmail.com>
2016-01-24 13:39:22 -0500

    Created CityWrapper

---
+++ src/com/util/CityWrapper.java
@@ -0,0 +1,60 @@
+/**
+ * CityWrapper: Class to store the City names and the distance between them
+ */
+package com.util;
+
+/**
+ * @author ankit-arch
+ *
+ */
+public class CityWrapper {
+	private String city1, city2;
+	private int distance;
+	
+	// initializing all variables with empty values
+	CityWrapper() {
+		city1="";
+		city2="";
+		distance=0;
+	}
+	
+	/**
+	 * @return the city1
+	 */
+	public String getCity1() {
+		return city1;
+	}
+	/**
+	 * @param city1 the city1 to set
+	 */
+	public void setCity1(String city1) {
+		this.city1 = city1;
+	}
+	/**
+	 * @return the city2
+	 */
+	public String getCity2() {
+		return city2;
+	}
+	/**
+	 * @param city2 the city2 to set
+	 */
+	public void setCity2(String city2) {
+		this.city2 = city2;
+	}
+	/**
+	 * @return the distance
+	 */
+	public int getDistance() {
+		return distance;
+	}
+	/**
+	 * @param distance the distance to set
+	 */
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+	
+	
+
+}
