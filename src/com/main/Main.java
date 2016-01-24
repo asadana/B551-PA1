9d18d7763a0e53ac520244fd0da177ab2d88fc8a
Ankit Sadana <sadana.ankit@gmail.com>
2016-01-24 13:27:02 -0500

    Initialized project

---
+++ src/com/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.main;
+
+/**
+ * @author ankit-arch
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
