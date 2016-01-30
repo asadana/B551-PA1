8203a0a1116c1a9dc0cc006d19e4e874288c41bf
Ankit Sadana <sadana.ankit@gmail.com>
2016-01-29 20:17:12 -0500

    Hardcoded default file and moved delim to main

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,5 +1,8 @@
 /**
+ * Main: Inputs the file from the user and passes it to ReadFile
  * 
+ * Part of Programming Assignment #1 for B551
+ * Spring 2016
  */
 package com.main;
 
@@ -10,34 +13,42 @@ import java.io.InputStreamReader;
 import com.util.ReadFile;
 
 /**
- * @author ankit-arch
+ * @author asadana, jaynagle
  *
  */
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) throws IOException {
+		// BufferedReader to read and store user input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String fileName;
 		File fileDist;
 		
+		// fileName stores the fileName hardcoded or entered by the user
+		String fileName = "romania-distance.txt";
+		// delim stores the delimiter used to parse the txt file
+		String delim = " ";
+		
+		// while loop checks if the file exists or asks user to enter another file
 		while (true) {
-			System.out.println("Enter the name of the file for cities: ");
-			fileName = br.readLine();
 			fileDist = new File (fileName);
 			
+			// If file exists
 			if (fileDist.exists()) {
 				System.out.println("\nFile Found!\n");
 				break;
 			}
+			// If file not found
 			else {
 				System.out.println("\nERROR: File doesn't exist.\nPlease try again.\n");
 			}
+			
+			// If file is not found, user is asked for a different file name in the parent directory
+			System.out.println("Enter the name of the file for cities: ");
+			fileName = br.readLine();
 		} 
 		
-		ReadFile readFileObj = new ReadFile(fileName);
+		// ReadFile reads the file given
+		ReadFile readFileObj = new ReadFile(fileName, delim);
 		readFileObj.readIt();
 		
 		br.close();
