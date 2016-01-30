a8c77a20ccc88c0f5411feb5d6fe6cc4583da04a
Ankit Sadana <sadana.ankit@gmail.com>
2016-01-29 20:17:52 -0500

    Removed delim from ReadFile. Added comments

--- src/com/util/CityMapper.java
+++ src/com/util/CityMapper.java
@@ -1,18 +1,24 @@
 /**
+ * CitMapper: This class holds each unique City and it's corresponding neighbors with distances
  * 
+ * Part of Programming Assignment #1 for B551
+ * Spring 2016
  */
 package com.util;
 
 import java.util.HashMap;
 
 /**
- * @author ankit-arch
+ * @author asadana, jaynagle
  *
  */
 public class CityMapper {
 	private String cityName;
+	// The HashMap contains name of the neighbor and it's distance
 	private HashMap<String, Integer> neighbors;
 	
+	// Parameterized constructor
+	// Meant for a new CityMapper Obj, initializing with a new neighbor and it's distance
 	CityMapper(String cityName, String neighborName, int distance) {
 		this.cityName = cityName;
 		neighbors = new HashMap<String, Integer>();
--- src/com/util/ReadFile.java
+++ src/com/util/ReadFile.java
@@ -1,3 +1,10 @@
+/**
+ * ReadFile: Reads the given file, parses and stores the information in CityMapper arrayList
+ * File is assumed: City1 City2 distance
+ * 
+ * Part of Programming Assignment #1 for B551
+ * Spring 2016
+*/
 package com.util;
 
 import java.io.BufferedReader;
@@ -6,50 +13,80 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+/**
+ * @author asadana, jaynagle
+ *
+ */
 public class ReadFile {
+	// fileName is received from Main
 	private String fileName;
+	// delim is the delimiter used to parse the txt file, received from Main
 	private String delim;
+	// CityMapper ArrayList to store all the cities and their neighbors
 	private ArrayList<CityMapper> cityList;
-	//private ArrayList<CityWrapper> cityList;
 	
-	public ReadFile (String fileName) {
+	// Parameterized constructor
+	// Receives fileName and delim and initializes a new ArrayList
+	public ReadFile (String fileName, String delim) {
 		this.setFileName(fileName);
-		this.setDelim(" ");
+		this.setDelim(delim);
 		cityList = new ArrayList<CityMapper>();
 	}
 	
+	// readIt is a functions which reads and parses the fileName file.
 	public void readIt() throws IOException {
+		// Objects to read the file
 		FileInputStream fileInputObj = new FileInputStream(fileName);
 		BufferedReader br = new BufferedReader(new InputStreamReader(fileInputObj));
 		String lineRead;
+		
+		// booleans to check if the City1 or City2 are already in the list
 		boolean foundCity1Bool = false, foundCity2Bool = false;
+		// CityMapper object to create and add objects to cityList
 		CityMapper cityMapperObj;
 		
+		// while loop reads the one line at a time till end of file
 		while ((lineRead = br.readLine()) != null) {
+			// split the read line with delim
 			String[] temp = lineRead.split(delim);
+			// resetting the booleans to false
 			foundCity1Bool = false;
 			foundCity2Bool = false;
 			
+			// if the arrayList is empty, add both City1 and City2 to the cityList
 			if(cityList.isEmpty()) {
 				cityMapperObj = new CityMapper(temp[0], temp[1], Integer.parseInt(temp[2]));
 				cityList.add(cityMapperObj);
 				cityMapperObj = new CityMapper(temp[1], temp[0], Integer.parseInt(temp[2]));
 				cityList.add(cityMapperObj);
-			} else {
+			}
+			// If cityList is not empty
+			else {
+				// Checking the cityList if City1 and/or City2 is already added
 				for (CityMapper cityObj : cityList) {
+					
+					// Check for City1 in the list
+					// If found, add a new neighbor
 					if(temp[0].compareTo(cityObj.getCityName()) == 0) {
 						cityObj.addNeighbors(temp[1], Integer.parseInt(temp[2]));
 						foundCity1Bool = true;
 					}
+					
+					// Check for City2 in the list
+					// If found, add a new neighbor
 					else if(temp[1].compareTo(cityObj.getCityName()) == 0) {
 						cityObj.addNeighbors(temp[0], Integer.parseInt(temp[2]));
 						foundCity2Bool = true;
 					}
 				}
+				
+				// If City1 wasn't found, add it to the list
 				if(!foundCity1Bool) {
 					cityMapperObj = new CityMapper(temp[0], temp[1], Integer.parseInt(temp[2]));
 					cityList.add(cityMapperObj);
 				}
+				
+				// If City2 wasn't found, add it to the list
 				if(!foundCity2Bool) {
 					cityMapperObj = new CityMapper(temp[1], temp[0], Integer.parseInt(temp[2]));
 					cityList.add(cityMapperObj);
@@ -58,6 +95,7 @@ public class ReadFile {
 		}
 		br.close();
 		
+		// Display all cities in cityList and it's neighbors
 		for(CityMapper cityObj : cityList) {
 			System.out.println("City: " + cityObj.getCityName() + "\t" + cityObj.getNeighbors());
 		}
